6f14ac48b709c283494e5f7e281f03e3e447e004
bluesatch <satchwerk@gmail.com>
2023-09-27 14:37:52 -0500

    cleaned color theme function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 const themeBtn = document.getElementById('themeBtn')
 
 themeBtn.addEventListener('click', ()=> {
-    
     const html = document.getElementsByTagName('html')[0]
-
     const theme = html.getAttribute('data-bs-theme')
-
     theme === 'light' ? html.setAttribute('data-bs-theme', 'dark') : html.setAttribute('data-bs-theme', 'light')
-
-    
 })
