36e201da6376abe5eba8a3f1003599d4c34cf389
bluesatch <satchwerk@gmail.com>
2023-09-27 14:22:26 -0500

    deleted:    app.js 	modified:   css/style.css 	modified:   index.html 	new file:   js/app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const themeBtn = document.getElementById('themeBtn')
+
+themeBtn.addEventListener('click', ()=> {
+    
+    const html = document.getElementsByTagName('html')[0]
+
+    const theme = html.getAttribute('data-bs-theme')
+
+    theme === 'light' ? html.setAttribute('data-bs-theme', 'dark') : html.setAttribute('data-bs-theme', 'light')
+
+    console.log(theme)
+})
